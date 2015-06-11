6162043463e037645764b901fa298cf0903b75ed
Chintan Prajapati <chintan.prajapati@infostretch.com>
2015-06-11 14:46:00 +0530

    Initial Commit

---
+++ App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Chintan Prajapati on 11/06/15.
+//  Copyright (c) 2015 Infostretch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  App
+//
+//  Created by Chintan Prajapati on 11/06/15.
+//  Copyright (c) 2015 Infostretch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
